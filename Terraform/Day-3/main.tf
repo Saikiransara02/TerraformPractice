de482f3c725d87f170f92437046fb18e8c1a903b
SAI KIRAN SARA <sarasaikiran02@gmail.com>
2025-04-16 11:50:19 +0530

    Day-3

---
+++ Terraform/Day-3/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  
+}
+
+resource "aws_instance" "SSK" {
+    ami = "ami-002f6e91abff6eb96"
+    instance_type = "t2.micro"
+    key_name = "Terraformkey"
+
+    tags = {
+    Name = "ec2SSk"
+  }
+    
+}
